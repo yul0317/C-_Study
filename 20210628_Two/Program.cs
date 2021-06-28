5dcf01009d8aa6726ead2d0dc3bc2fc0d1e79ec9
yul0317 <68114089+yul0317@users.noreply.github.com>
2021-06-28 17:38:32 +0900

    20210628_1738

---
+++ 20210628_Two/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _20210628_Two
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = -1;
+
+            while (N <= 1 || N >= 1000000)
+            {
+                N = Convert.ToInt32(Console.ReadLine());
+            }
+
+            int num_flag = -1;
+            int[] array = new int[N];
+            while (num_flag == -1)
+            {
+                
+                string[] inputNumber = Console.ReadLine().Split();
+                for (int i = 0; i < N; i++)
+                {
+                    array[i] = Convert.ToInt32(inputNumber[i]);
+                }
+                foreach (int i in array)
+                {
+                    if (i < -1000000 || i > 1000000)
+                    {
+                        num_flag = -1;
+                    }
+                    else
+                    {
+                        num_flag = 1;
+                    }
+                }
+            }
+
+            Array.Sort(array);
+            Console.WriteLine(array[0]+" "+array[N-1]);
+            
+
+        }
+    }
+}
---
+++ 20210628_Two/back_2557.cs
@@ -0,0 +1,8 @@
+using System;
+
+public class back_2557
+{
+    public back_2557(){
+        Console.WriteLine("Hello World!");
+    }
+}
